a198500d4c4e2607bb2e7fd23440594e287205f2
Priyanka <priyankahansi95@gmail.com>
2022-05-19 20:44:10 +0530

    added:.gitignore

---
+++ src/com/brigelabz/Firstname.java
@@ -0,0 +1,22 @@
+package com.brigelabz;
+
+import java.util.regex.Pattern;
+
+public interface Firstname {
+    void validUser(String name);
+    }
+    class RegexLambdaExpression {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration program");
+        Firstname function = (name) -> {
+            boolean check = Pattern.matches("([A-Z][a-z]{3,})", name);
+            if (check == true) {
+                System.out.println("Firstname is Valid");
+            } else
+                System.out.println("Enter valid name");
+        };
+        function.validUser("Praju");
+    }
+}
+
