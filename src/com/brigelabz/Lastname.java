a3d521b31c62927c045762ecdc6159ab33c53b46
Priyanka <priyankahansi95@gmail.com>
2022-05-19 21:08:34 +0530

    added:Password rule 2

--- src/com/brigelabz/Lastname.java
+++ src/com/brigelabz/Lastname.java
@@ -2,25 +2,23 @@ package com.brigelabz;
 
 import java.util.regex.Pattern;
 @FunctionalInterface
-interface PasswordRule1 {
-    void validUser(String password);
-}
+interface PasswordRule2 {
+        void validUser(String password);
+        }
 
 class RegexLambdaExpression {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to User Registration program");
-        PasswordRule1 function = (password)->{
-            boolean check = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&]).{8,}$",password);
+        PasswordRule2 function = (password) -> {
+            boolean check = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$", password);
             if (check == true) {
                 System.out.println("Password is Valid");
-            }else
+            } else
                 System.out.println("Enter valid password");
         };
         function.validUser("Praju@123");
     }
 }
-
 
 
 
