b38e985502f282f6e131d652895c3c013ac48ed0
Priyanka <priyankahansi95@gmail.com>
2022-05-20 19:24:25 +0530

    added:Password rules

--- src/com/brigelabz/Lastname.java
+++ src/com/brigelabz/Lastname.java
@@ -2,13 +2,13 @@ package com.brigelabz;
 
 import java.util.regex.Pattern;
 @FunctionalInterface
-interface PasswordRule2 {
+interface PasswordRule4 {
     void validUser(String password);
 }
  class RegexLambdaExpression {
 
     public static void main(String[] args) {
-        PasswordRule2 function = (password)->{
+        PasswordRule4 function = (password)->{
             boolean check = Pattern.matches("^(?=.*[0-9]{1,})(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$",password);
             if (check == true) {
                 System.out.println("Password is Valid");
@@ -18,6 +18,8 @@ interface PasswordRule2 {
         function.validUser("Praju@1234");
     }
 }
+
+
 
 
 
