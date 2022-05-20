761f7684da996e403a781c5c373669f1a3a977dd
Priyanka <priyankahansi95@gmail.com>
2022-05-20 19:13:27 +0530

    added:Password3 Rules

--- src/com/brigelabz/Lastname.java
+++ src/com/brigelabz/Lastname.java
@@ -3,20 +3,19 @@ package com.brigelabz;
 import java.util.regex.Pattern;
 @FunctionalInterface
 interface PasswordRule2 {
-        void validUser(String password);
-        }
-
-class RegexLambdaExpression {
+    void validUser(String password);
+}
+ class RegexLambdaExpression {
 
     public static void main(String[] args) {
-        PasswordRule2 function = (password) -> {
-            boolean check = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$", password);
+        PasswordRule2 function = (password)->{
+            boolean check = Pattern.matches("^(?=.*[0-9]{1,})(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$",password);
             if (check == true) {
                 System.out.println("Password is Valid");
-            } else
-                System.out.println("Enter valid password");
+            }else
+                System.out.println("Password Is Invalid");
         };
-        function.validUser("Praju@123");
+        function.validUser("Praju@1234");
     }
 }
 
